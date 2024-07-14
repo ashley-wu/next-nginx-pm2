f72305cb9fe42cbce0f5fa6051f9437eea924e22
Ashley Wu <ashley@lustertech.net>
2024-07-14 17:36:32 +0800

    Add PM2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+    script: 'npm start',
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      key: 'next-nginx-pm2-key.pem',
+      user : 'ubuntu',
+      host : '54.238.125.170',
+      ref  : 'origin/main',
+      repo : 'git@github.com/ashley-wu/next-nginx-pm2.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
